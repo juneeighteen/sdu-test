59ea1b8d9ce62335aa64cdeae20ae414e0177089
juneeighteen <develop.github@juneeighteen.com>
2014-11-05 09:15:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 chmod +x *.sh
+chmod 700 /root/gerritKey
 
 echo "bootstrap.sh called to install Ansible"
 echo "installing python-yaml"
@@ -31,7 +32,16 @@ source ./hacking/env-setup
 echo "Writing localhost to ~/ansible_hosts"
 echo "localhost ansible_connection=local" > ~/ansible_hosts
 
-exit 0
+#Currently gitProvision.yml may contain sensitive information, so it is copied in locally
+cp /root/gitProvision.yml /root/sdu-test/provisions/gitProvision.yml
+
 # echo "Is Ansible alive? Ping/Pong Request"
 # ansible -i ~/ansible_hosts all -m ping
 
+/root/ansible/bin/ansible-playbook /root/sdu-test/provisions/usersProvision.yml -i /root/ansible_hosts
+/root/ansible/bin/ansible-playbook /root/sdu-test/provisions/fileProvision.yml -i /root/ansible_hosts
+/root/ansible/bin/ansible-playbook /root/sdu-test/provisions/aptProvision.yml -i /root/ansible_hosts
+/root/ansible/bin/ansible-playbook /root/sdu-test/provisions/pipProvision.yml -i /root/ansible_hosts
+/root/ansible/bin/ansible-playbook /root/sdu-test/provisions/gitProvision.yml -i /root/ansible_hosts
+
+exit 0
